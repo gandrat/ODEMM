4e3b131a8768ce23f9740fd16c71dcd79f3bab0b
gandrat <gandratia@gmail.com>
2021-02-10 11:15:13 -0300

    Adding some comments to script.

--- 00_stage1.R
+++ 00_stage1.R
@@ -1,57 +1,59 @@
+#Script made to transform raw spreadsheet into DF used to create ODEMM horrendograms
+#Made by Tiago Gandra <prof.tiago.gandra@gmail.com>
+
 require(dplyr)
 require(readODS)
 
 rm(list=ls()) #removing previous objects
 
+#Loading tables from ODS spreadsheet
 pressures<-read_ods("Data/stage1.ods",sheet='pressures')
-
-
 ecochar<-read_ods("Data/stage1.ods",sheet='ecochars')
-
-
 sectors<-read_ods("Data/stage1.ods",sheet='sectors')
 
 
 secpress<-read_ods("Data/stage1.ods",sheet='secPress_clean')
 rownames(secpress)<-secpress$pressure
 secpress<-secpress%>%select(-pressure)
-lsecpress <- data.frame(rows = rownames(secpress), stack(secpress))
-names(lsecpress)<-c('idpress','secpress','idsec')
-lsecpress<-lsecpress%>%filter(secpress==1)%>%select(-secpress)
+
 
 ecopress<-read_ods("Data/stage1.ods",sheet='ecopress_clean')
 rownames(ecopress)<-ecopress$ecochar
 ecopress<-ecopress%>%select(-ecochar)
-lecopress <- data.frame(rows = rownames(ecopress), stack(ecopress))
-names(lecopress)<-c('ideco','ecopress','idpress')
-lecopress<-lecopress%>%filter(ecopress==1)%>%select(-ecopress)
-
-
 
 sececo<-read_ods("Data/stage1.ods",sheet='SecEco_clean')
 rownames(sececo)<-sececo$sector
 sececo<-sececo%>%select(-sector)
 
+#Transforming DF in long tables with relationships
+lsecpress <- data.frame(rows = rownames(secpress), stack(secpress))
+names(lsecpress)<-c('idpress','secpress','idsec')
+lsecpress<-lsecpress%>%filter(secpress==1)%>%select(-secpress)
+
+lecopress <- data.frame(rows = rownames(ecopress), stack(ecopress))
+names(lecopress)<-c('ideco','ecopress','idpress')
+lecopress<-lecopress%>%filter(ecopress==1)%>%select(-ecopress)
+
 lsececo <- data.frame(rows = rownames(sececo), stack(sececo))
 names(lsececo)<-c('idsec','sececo','ideco')
 lsececo<-lsececo%>%filter(sececo==1)%>%select(-sececo)
 
+
+#Merging long tables into one table
 all<-merge(lecopress,lsecpress,by='idpress')
 all<-merge(all,lsececo, by=c('ideco','idsec'))
 
-
-table(all$ideco)
-table(all$idsec)
-table(all$idpress)
-
-
 all<-all%>%arrange(idsec,ideco,idpress)
 
+#Adding names to table (instead of pressures, ecochar and sectors codes)
 all<-merge(all,sectors,by='idsec')
 all<-merge(all,pressures,by='idpress')
 all<-merge(all,ecochar,by='ideco')
 
+#Creating fake values for scoring
 raw<-all%>%transmute(Sector=sector, Pressure=pressure, Ecological.Characteristic=ecochar,
                              Overlap="S", Overlap.Score=.1,Frequency.Score=.1,DoI.Score=.1,Resilience.Score=.1,Persistence.Score=.1)
+
+#Saving data to use in shiny app
 save(raw,pressures,sectors,ecochar,file='Data/raw.Rda')
 
