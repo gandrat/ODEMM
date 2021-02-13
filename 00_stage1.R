2d0a469546d7c729b1da0d5d41812e2026f5b1a8
gandrat <gandratia@gmail.com>
2021-02-13 19:34:35 -0300

    Optimizing script.

--- 00_stage1.R
+++ 00_stage1.R
@@ -28,32 +28,46 @@ sececo<-sececo%>%select(-sector)
 #Transforming DF in long tables with relationships
 lsecpress <- data.frame(rows = rownames(secpress), stack(secpress))
 names(lsecpress)<-c('idpress','secpress','idsec')
-lsecpress<-lsecpress%>%filter(secpress==1)%>%select(-secpress)
+# lsecpress<-lsecpress%>%filter(secpress==1)%>%select(-secpress)
 
 lecopress <- data.frame(rows = rownames(ecopress), stack(ecopress))
 names(lecopress)<-c('ideco','ecopress','idpress')
-lecopress<-lecopress%>%filter(ecopress==1)%>%select(-ecopress)
+# lecopress<-lecopress%>%filter(ecopress==1)%>%select(-ecopress)
 
 lsececo <- data.frame(rows = rownames(sececo), stack(sececo))
 names(lsececo)<-c('idsec','sececo','ideco')
-lsececo<-lsececo%>%filter(sececo==1)%>%select(-sececo)
+# lsececo<-lsececo%>%filter(sececo==1)%>%select(-sececo)
 
 
 #Merging long tables into one table
 all<-merge(lecopress,lsecpress,by='idpress')
 all<-merge(all,lsececo, by=c('ideco','idsec'))
 
-all<-all%>%arrange(idsec,ideco,idpress)
+all<-all%>%arrange(idsec,idpress,ideco)
 
 #Adding names to table (instead of pressures, ecochar and sectors codes)
 all<-merge(all,sectors,by='idsec')
 all<-merge(all,pressures,by='idpress')
 all<-merge(all,ecochar,by='ideco')
 
+stage2<-all%>%transmute(idsec=as.numeric(substring(idsec,2,3)), sector=sector, idpress=as.numeric(substring(idpress,2,3)), pressure=pressure, 
+                        ideco=as.numeric(substring(ideco,2,3)), ecochar=ecochar, 
+                        overlap= case_when(secpress==1 & secpress==1 & sececo==1 ~ 'S'))
+
+stage2[is.na(stage2)]<-'NO'
+
+
+stage2<-stage2%>%arrange(idsec,idpress,ideco)
+
+nrow(stage2%>%filter(overlap=='S'))
+#Saving table stage2
+write.csv(stage2, "output_data/stage2.csv")
+
 #Creating fake values for scoring
-raw<-all%>%transmute(Sector=sector, Pressure=pressure, Ecological.Characteristic=ecochar,
-                             Overlap="S", Overlap.Score=.1,Frequency.Score=.1,DoI.Score=.1,Resilience.Score=.1,Persistence.Score=.1)
+raw<-stage2%>%filter(overlap=='S')%>%
+  transmute(Sector=sector, Pressure=pressure, Ecological.Characteristic=ecochar,
+                             Overlap=overlap, Overlap.Score=.1,Frequency.Score=.1,DoI.Score=.1,Resilience.Score=.1,Persistence.Score=.1)
 
 #Saving data to use in shiny app
-save(raw,pressures,sectors,ecochar,file='Data/raw.Rda')
+save(raw,file='Data/raw.Rda')
 
